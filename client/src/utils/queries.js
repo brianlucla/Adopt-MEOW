ac2b3d9e9f3c0d7bf30a7da47602aefc253e8ddf
brianlu18 <brianlu18@g.ucla.edu>
2023-08-04 12:53:35 -0500

    finished this time fr

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -29,3 +29,37 @@ export const GET_ME = gql`
     }
   }
 `;
+
+export const GET_FAVORITES = gql`
+  {
+    me {
+      favorites {
+        animalID
+        type
+        breed
+        name
+        colors
+        age
+        size
+        photoURL
+      }
+    }
+  }
+`;
+
+export const GET_ADOPTED = gql`
+  {
+    me {
+      adopted {
+        animalID
+        type
+        breed
+        name
+        colors
+        age
+        size
+        photoURL
+      }
+    }
+  }
+`;
