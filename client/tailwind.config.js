016ae1d6c21f8af490bf66f061f1e116f5abab16
Merika <merikatoby143@gmail.com>
2023-08-04 10:04:26 -0700

    tailwind cust colors + media queeries hook

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,9 +1,46 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./src/**/*.{html,js,jsx}"],
+  mode: "jit",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: "#0074d9",
+        pink: "#F500A2",
+        teal: "#6EE2F5",
+        yellow: "#FEC601",
+        grey: "#EDEDED",
+        "deep-blue": "#010026",
+        "dark-grey": "#757575",
+        "opaque-black": "rgba(0,0,0,0.35)"
+      },
+      backgroundImage: (theme) => ({
+        "gradient-rainbow":
+          "linear-gradient(81.66deg, #00B5EE 7.21%, #FF45A4 45.05%, #FFBA00 78.07%)",
+
+        "gradient-rainblue":
+          "linear-gradient(90deg, #24CBFF 14.53%, #FC59FF 69.36%, #FFBD0C 117.73%)",
+      }),
+      fontFamily: {
+        playfair: ["Playfair Display", "serif"],
+        opensans: ["Open Sans", "sans-serif"],
+      },
+      content: {
+        // brush: "url('./assets/brush.png')",
+        beef: "url('./assets/images/BEEF.jpg')",
+        winnie: "url('./BBYJEFF.jpg')",
+        luna: "url('./assets/LUNA.png')",
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
   },
   plugins: [],
-}
+};
 
