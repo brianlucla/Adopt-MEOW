c98691c40e5d9424791dfb0faf67602922b05f47
Merika <merikatoby143@gmail.com>
2023-08-07 10:15:09 -0700

    animal images added

--- client/src/components/DogsPage.jsx
+++ client/src/components/DogsPage.jsx
@@ -2,6 +2,19 @@ import React from "react";
 import Navbar from "./Navbar";
 import Banner from "./Banner";
 import Card from "./Card";
+import Max from "../assets/images/Dogs/Max.png";
+import Bethany from "../assets/images/Dogs/Bethany.png";
+import Rocky from "../assets/images/Dogs/Rocky.png";
+import LUNA from "../assets/images/Dogs/LUNA.png";
+import Duke from "../assets/images/Dogs/Duke.png";
+import Coco from "../assets/images/Dogs/Coco.png";
+import Martin from "../assets/images/Dogs/Martin.png";
+import Dexter from "../assets/images/Dogs/Dexter.png";
+import Zeus from "../assets/images/Dogs/Zeus.png";
+import Bruno from "../assets/images/Dogs/Bruno.png";
+import Dutches from "../assets/images/Dogs/Dutches.png";
+import Sadie from "../assets/images/Dogs/Sadie.png";
+
 
 const DogsPage = () => {
   const dogs = [
@@ -10,72 +23,84 @@ const DogsPage = () => {
       age: "3 years",
       breed: "Golden Retriever",
       weight: "70 lbs",
+      image: Max,
     },
     {
       name: "Bethany",
       age: "2 years",
       breed: "Labrador Retriever",
       weight: "80 lbs",
+      image: Bethany,
     },
     {
       name: "Rocky",
       age: "4 years",
       breed: "German Shepard",
       weight: "80 lbs",
+      image: Rocky,
     },
     {
       name: "LUNA",
       age: "1.5 years",
       breed: "Siberian Husky",
       weight: "55 lbs",
+      image: LUNA, 
     },
     {
       name: "Duke Wellington",
       age: "5 years",
       breed: "Beagle",
       weight: "30 lbs",
+      image: Duke, 
     },
     {
       name: "Coco",
       age: "2 years",
       breed: "French Bulldog",
       weight: "25 lbs",
+      image: Coco,
     },
     {
       name: "Martin",
       age: "6 years",
       breed: "Poodle",
       weight: "15 lbs",
+      image: Martin, 
     },
     {
       name: "Dexter",
       age: "3 years",
       breed: "Border Collie",
       weight: "40 lbs",
+      image: Dexter, 
     },
     {
       name: "Zeus",
       age: "4 years",
       breed: "Rottweiler",
       weight: "90 lbs",
+      image: Zeus, 
     },
     {
       name: "Bruno",
       age: "2.5 years",
       breed: "Dachshund",
       weight: "12 lbs",
+      image: Bruno,
     },
     {
       name: "Dutches",
       age: "1 years",
       breed: "Bulldog",
       weight: "50 lbs",
+      image: Dutches, 
     },
     {
       name: "Sadie",
       age: "3.5 years",
       breed: "Boxer",
       weight: "65 lbs",
+      image: Sadie,
     },
   ];
 
--- client/src/components/RabbitsPage.jsx
+++ client/src/components/RabbitsPage.jsx
@@ -2,6 +2,19 @@ import React from "react";
 import Navbar from "./Navbar";
 import Banner from "./Banner";
 import Card from "./Card";
+import Thumper from "../assets/rabbits/Thumper.png";
+import Snowflake from "../assets/rabbits/Snowflake.png";
+import Oreo from "../assets/rabbits/Oreo.png";
+import Cotton from "../assets/rabbits/Cottontail.png";
+import Marshmallow from "../assets/rabbits/Marshmallow.png";
+import Peanut from "../assets/rabbits/Peanut.png";
+import Honey from "../assets/rabbits/Honey.png";
+import Midnight from "../assets/rabbits/Midnight.png";
+import Ruby from "../assets/rabbits/Ruby.png";
+import Gizmo from "../assets/rabbits/Gizmo.png";
+import Clover from "../assets/rabbits/Clover.png";
+import Jasper from "../assets/rabbits/Jasper.png";
+
 
 const RabbitsPage = () => {
   const rabbits = [
@@ -10,72 +23,84 @@ const RabbitsPage = () => {
       age: "1.5 years",
       breed: "Holland Lop",
       weight: "4 lbs",
+      image: Thumper,
     },
     {
       name: "Snowflake",
       age: "2 years",
       breed: "Mini Rex",
       weight: "3.5 lbs",
+      image: Snowflake,
     },
     {
       name: "Oreo",
       age: "3 years",
       breed: "Netherland Dwarf",
       weight: "2.5 lbs",
+      image: Oreo,
     },
     {
       name: "Cottontail",
       age: "1 years",
       breed: "Lionhead",
       weight: "5 lbs",
+      image: Cotton,
     },
     {
       name: "Marshmallow",
       age: "4 years",
       breed: "English Lop",
       weight: "6 lbs",
+      image: Marshmallow, 
     },
     {
       name: "Peanut",
       age: "2.5 years",
       breed: "Dwarf Hotot",
       weight: "3 lbs",
+      image: Peanut,
     },
     {
       name: "Honey",
       age: "3 years",
       breed: "Flemish Giant",
       weight: "12 lbs",
+      image: Honey, 
     },
     {
       name: "Midnight",
       age: "2 years",
       breed: "Mini Lop",
       weight: "4.5 lbs",
+      image: Midnight,
     },
     {
       name: "Ruby",
       age: "4 years",
       breed: "Californian",
       weight: "8 lbs",
+      image: Ruby,
     },
     {
       name: "Gizmo",
       age: "2.5 years",
       breed: "Rex",
       weight: "5.5 lbs",
+      image: Gizmo,
     },
     {
       name: "Clover",
       age: "1 years",
       breed: "Polish",
       weight: "2 lbs",
+      image: Clover, 
     },
     {
       name: "Jasper",
       age: "3.5 years",
       breed: "Harlequin",
       weight: "7 lbs",
+      image: Jasper, 
     },
   ];
 
